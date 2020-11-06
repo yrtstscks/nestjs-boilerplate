ad3849630d63056f821045883ab8a3d6d005093d
dioio-io <dio@dioio.io>
2020-11-06 14:24:59 +0000

    first commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,7 +5,7 @@ export default {
   host: process.env.DB_HOST || 'postgres',
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_DATABASE_NAME || 'entities_service',
+  database: process.env.DB_DATABASE_NAME || 'boilerplate',
   port: Number(process.env.DB_PORT || 5432),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
